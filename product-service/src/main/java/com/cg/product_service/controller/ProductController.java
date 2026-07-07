05b17d0d8f8fa506d89aa25cacb7e2532f3ae2d4
Rahul Rathod <rahulrathod6203@gmail.com>
2026-07-07 11:35:49 +0530

    Product controller added with greet endpoint

---
+++ product-service/src/main/java/com/cg/product_service/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.cg.product_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    @GetMapping("/home")
+    public String greet(){
+        return "Welcome";
+    }
+
+
+}
